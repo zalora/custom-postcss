bce670be63d4ac0a764f99cb8c4cc96b43c391fe
Tri Tran <thuutri2710@gmail.com>
2024-05-30 17:22:04 +0700

    Update rules

--- index.js
+++ index.js
@@ -10,6 +10,64 @@ module.exports = function myPostCSSPlugin() {
             ":not([hidden]) ~ :not([hidden])",
             ":not(:last-child)"
           );
+
+          const nodes = rule.nodes;
+
+          // update style for divide-y
+          if (rule.selector.includes("divide-y")) {
+            const bottomIndex = nodes.findIndex((node) => node.prop === "border-bottom-width");
+            const topIndex = nodes.findIndex((node) => node.prop === "border-top-width");
+
+            if (bottomIndex !== -1) {
+              nodes[bottomIndex].prop = "border-top-width";
+            }
+
+            if (topIndex !== -1) {
+              nodes[topIndex].prop = "border-bottom-width";
+            }
+          }
+
+          // update style for divide-x
+          if (rule.selector.includes("divide-x")) {
+            const endIndex = nodes.findIndex((node) => node.prop === "border-inline-end-width");
+            const startIndex = nodes.findIndex((node) => node.prop === "border-inline-start-width");
+
+            if (endIndex !== -1) {
+              nodes[endIndex].prop = "border-inline-start-width";
+            }
+
+            if (startIndex !== -1) {
+              nodes[startIndex].prop = "border-inline-end-width";
+            }
+          }
+
+          // update style for space-x
+          if (rule.selector.includes("divide-x")) {
+            const endIndex = nodes.findIndex((node) => node.prop === "margin-inline-end");
+            const startIndex = nodes.findIndex((node) => node.prop === "margin-inline-start");
+
+            if (endIndex !== -1) {
+              nodes[endIndex].prop = "margin-inline-start";
+            }
+
+            if (startIndex !== -1) {
+              nodes[startIndex].prop = "margin-inline-end";
+            }
+          }
+
+          // update style for space-y
+          if (rule.selector.includes("divide-x")) {
+            const bottomIndex = nodes.findIndex((node) => node.prop === "margin-bottom");
+            const topIndex = nodes.findIndex((node) => node.prop === "margin-top");
+
+            if (endIndex !== -1) {
+              nodes[endIndex].prop = "margin-top";
+            }
+
+            if (startIndex !== -1) {
+              nodes[startIndex].prop = "margin-bottom";
+            }
+          }
         }
       });
     },
