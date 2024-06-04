00d91e72e75fddd9cd6d241aa50e805a21110542
Tri Tran <thuutri2710@gmail.com>
2024-06-04 13:01:02 +0700

    Update rule

--- index.js
+++ index.js
@@ -11,63 +11,63 @@ module.exports = function myPostCSSPlugin() {
             ":not(:last-child)"
           );
 
-          const nodes = rule.nodes;
+          // const nodes = rule.nodes;
 
-          // update style for divide-y
-          if (rule.selector.includes("divide-y")) {
-            const bottomIndex = nodes.findIndex((node) => node.prop === "border-bottom-width");
-            const topIndex = nodes.findIndex((node) => node.prop === "border-top-width");
+          // // update style for divide-y
+          // if (rule.selector.includes("divide-y")) {
+          //   const bottomIndex = nodes.findIndex((node) => node.prop === "border-bottom-width");
+          //   const topIndex = nodes.findIndex((node) => node.prop === "border-top-width");
 
-            if (bottomIndex !== -1) {
-              rule.nodes[bottomIndex].prop = "border-top-width";
-            }
+          //   if (bottomIndex !== -1) {
+          //     rule.nodes[bottomIndex].prop = "border-top-width";
+          //   }
 
-            if (topIndex !== -1) {
-              rule.nodes[topIndex].prop = "border-bottom-width";
-            }
-          }
+          //   if (topIndex !== -1) {
+          //     rule.nodes[topIndex].prop = "border-bottom-width";
+          //   }
+          // }
 
-          // update style for divide-x
-          if (rule.selector.includes("divide-x")) {
-            const endIndex = nodes.findIndex((node) => node.prop === "border-inline-end-width");
-            const startIndex = nodes.findIndex((node) => node.prop === "border-inline-start-width");
+          // // update style for divide-x
+          // if (rule.selector.includes("divide-x")) {
+          //   const endIndex = nodes.findIndex((node) => node.prop === "border-inline-end-width");
+          //   const startIndex = nodes.findIndex((node) => node.prop === "border-inline-start-width");
 
-            if (endIndex !== -1) {
-              rule.nodes[endIndex].prop = "border-inline-start-width";
-            }
+          //   if (endIndex !== -1) {
+          //     rule.nodes[endIndex].prop = "border-inline-start-width";
+          //   }
 
-            if (startIndex !== -1) {
-              rule.nodes[startIndex].prop = "border-inline-end-width";
-            }
-          }
+          //   if (startIndex !== -1) {
+          //     rule.nodes[startIndex].prop = "border-inline-end-width";
+          //   }
+          // }
 
-          // update style for space-x
-          if (rule.selector.includes("space-x")) {
-            const endIndex = nodes.findIndex((node) => node.prop === "margin-inline-end");
-            const startIndex = nodes.findIndex((node) => node.prop === "margin-inline-start");
+          // // update style for space-x
+          // if (rule.selector.includes("space-x")) {
+          //   const endIndex = nodes.findIndex((node) => node.prop === "margin-inline-end");
+          //   const startIndex = nodes.findIndex((node) => node.prop === "margin-inline-start");
 
-            if (endIndex !== -1) {
-              rule.nodes[endIndex].prop = "margin-inline-start";
-            }
+          //   if (endIndex !== -1) {
+          //     rule.nodes[endIndex].prop = "margin-inline-start";
+          //   }
 
-            if (startIndex !== -1) {
-              rule.nodes[startIndex].prop = "margin-inline-end";
-            }
-          }
+          //   if (startIndex !== -1) {
+          //     rule.nodes[startIndex].prop = "margin-inline-end";
+          //   }
+          // }
 
-          // update style for space-y
-          if (rule.selector.includes("space-y")) {
-            const bottomIndex = nodes.findIndex((node) => node.prop === "margin-bottom");
-            const topIndex = nodes.findIndex((node) => node.prop === "margin-top");
+          // // update style for space-y
+          // if (rule.selector.includes("space-y")) {
+          //   const bottomIndex = nodes.findIndex((node) => node.prop === "margin-bottom");
+          //   const topIndex = nodes.findIndex((node) => node.prop === "margin-top");
 
-            if (bottomIndex !== -1) {
-              rule.nodes[bottomIndex].prop = "margin-top";
-            }
+          //   if (bottomIndex !== -1) {
+          //     rule.nodes[bottomIndex].prop = "margin-top";
+          //   }
 
-            if (topIndex !== -1) {
-              rule.nodes[topIndex].prop = "margin-bottom";
-            }
-          }
+          //   if (topIndex !== -1) {
+          //     rule.nodes[topIndex].prop = "margin-bottom";
+          //   }
+          // }
         }
       });
     },
